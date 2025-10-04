59ba71ffcd900df3bf1b75a7c34b447a7c5cdcd4
Deacon Griffiths <dj4christ09@gmail.com>
2025-10-04 15:18:04 +0200

    Hello

--- 2025-2026/color_sensor_test.py
+++ 2025-2026/color_sensor_test.py
@@ -1,41 +1,15 @@
-# import cv2
-
-# t = 10
-
-# # width, height = 1024, 720
-
-# rp, gp, bp = 155, 43, 209
-# rg, gg, bg = 60, 176, 67
-# width, height = 640, 480  # use safe default
-# cap = cv2.VideoCapture(0)
-
-# # Check if camera opened successfully
-# if not cap.isOpened():
-#     print("Error: Could not open camera")
-#     exit()
-
-# cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
-# cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
-
-# while True:
-#     ret, img = cap.read()
-#     if not ret:
-#         print("Failed to capture frame")
-#         break
-
-#     cv2.imshow("preview", img)
-
-#     if cv2.waitKey(1) & 0xFF == ord("q"):
-#         break
 import cv2
+import numpy as np
 
-t = 2
+t = 50
+m = 127
 rp, gp, bp = 155, 43, 209
 rg, gg, bg = 60, 176, 67
 
 width, height = 640, 480  # use safe default
 
-cap = cv2.VideoCapture(2)
+
+cap = cv2.VideoCapture(0)
 
 while True:
     ar, ag, ab = 127, 127, 127
@@ -43,8 +17,6 @@ while True:
     ret, img = cap.read()
     if not ret:
         break
-
-    cv2.imshow("preview", img)
 
     ab, ag, ar, _ = cv2.mean(img)
 
@@ -57,14 +29,46 @@ while True:
     #         ag += g
     #         ab += b
 
-    
     b, g, r= img[round(height/2), round(width/2)]
-    b, g, r = round(b-ab), round(g-ag), round(r-ar)
-    print(r, g, b)
-
-    if (r >= ar-t and r <= ar+t) and (g >= ag-t and g <= ag+t) and (b >= ab-t and b <= ab+t):
-        print('Color recognized: purple')
     
+    color = (r, g, b)
+    solid_img = np.full((height, width, 3), color, dtype=np.uint8)
+    cv2.imshow("Middle Color", solid_img)
+
+    # print(f"{r} {g} {b}", end="\r")
+
+    # if ab >= m:
+    #     b = round(b-(ab-m))
+    # else:
+    #     b = round(b+(m+ab))
+    
+    # if ar >= m:
+    #     r = round(r-(ar-m))
+    # else:
+    #     r = round(r+(m+ar))
+    
+    # if ag >= m:
+    #     g = round(g-(ag-m))
+    # else:
+    #     g = round(g+(m+ag))
+
+
+    # color = (ar, ag, ab)
+    # solid_img = np.full((height, width, 3), color, dtype=np.uint8)
+    # cv2.imshow("Lighting", solid_img)
+
+    center_y, center_x = height // 2, width // 2
+    img[center_y, center_x] = (0, 0, 255)
+    cv2.imshow("preview", img)
+
+    if g > r-t and g > b-t:
+        print("green")
+    elif r > g-t and r > b-t:
+        print("red")
+    elif b > r-t and b > g-t:
+        print("blue")
+    else:
+        print("none")
     
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
